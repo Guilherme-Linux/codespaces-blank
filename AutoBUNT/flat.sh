1548f39386da9378de1e4ebff6aea65f96a85f61
Guilherme-Linux <oliverr.g@outlook.com>
2025-02-05 13:37:15 +0000

    first commit

---
+++ AutoBUNT/flat.sh
@@ -0,0 +1,4 @@
+flatpak install flathub org.inkscape.Inkscape -y 
+flatpak install flathub com.microsoft.Edge -y 
+flatpak install flathub com.google.Chrome -y 
+flatpak install flathub com.ktechpit.whatsie -y
